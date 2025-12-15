da28b9ddc8a197aa3eca254807dede9d7549eecc
TAM15937 <asbszhen.ru@gmail.com>
2025-12-15 15:42:39 +0700

    Add amd modify DTOs

---
+++ src/main/java/org/example/dto/DamageDetail.java
@@ -0,0 +1,9 @@
+package org.example.dto;
+
+public class DamageDetail {
+    private long offset;
+    private int originalByte;
+    private int damagedByte;
+    private String hexOriginal;
+    private String hexDamaged;
+}
--- src/main/java/org/example/dto/FileComparisonResult.java
+++ src/main/java/org/example/dto/FileComparisonResult.java
@@ -3,9 +3,12 @@ package org.example.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class FileComparisonResult {
     private String filename;
     private boolean damaged;
+    private List<DamageDetail> damages;
 }
